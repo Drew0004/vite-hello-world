ab2337984d3faeaa54e5c8714762272cd65b2964
Drew0004 <drdrewgraphic@gmail.com>
2024-01-17 14:40:30 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+
+export default {
+    data() {
+        return { 
+            count: 0 
+        }
+    },
+    components:{
+        AppTitle
+    }
+}
+</script>
+
+<template>
+    <div>
+        <AppTitle/>
+        
+        <h1>Prova</h1>
+
+        <p>{{ count }}</p>
+
+        <button @click="count++">
+            Incrementa counter
+        </button>
+
+    </div>
+</template>
+
+<style scoped>
+</style>
